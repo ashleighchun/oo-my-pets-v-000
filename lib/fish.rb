d79ed6984d1fdc7393effbf89f898f852feb49b2
Ashleigh Chun <ashleigh.chun@wework.com>
2018-12-11 01:22:46 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+  attr_reader :name, :owner
+  
+  attr_accessor :mood
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+  
+  def owner_name(owner)
+      self.owner.name
+    end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :species
+
+  
+  @@all = [] 
+  @@count = 0 
+  
+  def initialize(pets)
+    @species = "human"
+    
+  end
+    
+   
+    def say_species
+      "I am a #{@species}."
+    end 
+    
+   
+    
+    def pets
+      {:fishes => [], :dogs => [], :cats=> []}
+    end 
+    
+    def buy_fish(name)
+      Fish.owner = self
+      @@count += 1
+      self.pets
+      #can buy a fish that is an instance of the Fish class
+      #knows about its fishes 
+    end 
+    
+    def buy_cat
+      #can buy a cat that is an instance of the Cat class
+      #knows about its cats
+    end
+    
+    def buy_dog
+     #can buy a dog that is an instance of the Dog class
+      #knows about its dogs
+    end 
+    
+    def walk_dogs
+      #walks the dogs which makes the dogs' moods happy
+    end 
+    def play_with_cats
+      #plays with the cats which makes the cats moods happy
+    end 
+    
+    def feed_fish
+      #feeds the fishes which makes the fishes' moods happy
+    end 
+    
+    def sell_pets
+      #can sell all its pets, which make them nervous 
+    end
+    
+    def list_pets
+      #can list off its pets
+    end 
+  
 end
